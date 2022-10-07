66ebaf12388b56a78419198af1e0d2467c50b9dc
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-10-07 21:32:31 +0100

    mad rocketchat.js template

--- storefront/public/rocketchat.js
+++ storefront/public/rocketchat.js
@@ -1,6 +1,4 @@
-	(function(w, d, s, u) {
-		w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
-		var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
-		j.async = true; j.src = 'https://kayodekayode.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
-		h.parentNode.insertBefore(j, h);
-	})(window, document, 'script', 'https://kayodekayode.rocket.chat/livechat');
+// paste the immediately invoked code here
+(function () {
+	console.log("this is running")
+})();
